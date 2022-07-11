48f47bef52db01f82eea36cddb972c1d8e0372b8
wwpedro <pedrinhosilva35997@gmail.com>
2022-07-11 12:44:49 -0300

    conteudo da tela dicionado
    conteudo da tela dicionado, e alterações em andamento.

--- src/screens/Conteudo.js
+++ src/screens/Conteudo.js
@@ -18,6 +18,32 @@ export default function Conteudos() {
                         <Text onPress={()=>navigation.goBack()}>Voltar</Text>
                     </View>    
                 </View>
+                <View style={estilos.conteudoView}>
+                    <View style={estilos.sobreFilme}>
+                        <Text style={estilos.titulo}>
+                            Encanto
+                        </Text>
+                        <View style={estilos.texto}>            
+                            <Text style={estilos.textoConteudo}>
+                                <Text style={estilos.primeiraLetra}>V</Text>
+                                encedor do Globo de Ouro de animação e um dos favoritos na disputa 
+                                do Oscar deste ano, conta a história dos Madrigal, uma família extraordinária 
+                                que vive em um povoado na Colômbia. Cada membro tem um dom especial: força 
+                                excepcional, a capacidade de criar flores ou o poder de curar através da comida.
+                            </Text>
+                            <Image></Image>
+                        </View>
+                    </View>
+
+                    <View style={estilos.elenco}>
+                        <Text style={estilos.titulo}>Personagens</Text>
+                    </View>
+
+                    <View style={estilos.classificacao}>
+                        <Text style={estilos.titulo}>Classificação</Text>
+                        <View style={estilos.estrelas}></View>
+                    </View>
+                </View>
             </View>
 
         </ScrollView>
@@ -64,5 +90,52 @@ const estilos = StyleSheet.create({
         padding:20,
         margin:30,
         backgroundColor: "#D4D4D4"
-    }
+    },
+
+
+    conteudoView:{
+        flexDirection:"column",
+        alignItems:"center",
+        width:"100%"
+    },
+
+
+    sobreFilme:{
+        flexDirection:"column",
+        alignItems:"center"
+    },
+    titulo:{
+        color:"#ffffff",
+        fontSize: 40
+    },
+    texto:{
+        flexDirection: "row",
+        width:"80%",
+        alignItems:"center",
+        justifyContent:"center"
+    },
+    primeiraLetra:{
+        height:50,
+        width:50,
+        fontSize:50,
+        color:"#ffffff",
+        alignItems:"center",
+        justifyContent:"center",
+        position:"relative"
+
+    },
+    textoConteudo:{
+        fontSize:20,
+        color:"#ffffff"
+    },
+
+
+
+    elenco:{
+        marginTop:30,
+        height:"40%",
+        width:"100%",
+        backgroundColor:"red",
+        alignItems:"center"
+    },
 })
