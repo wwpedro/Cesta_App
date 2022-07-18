7888bb7bee3366c705e950b5b1ab366b7febecd7
wwpedro <pedrinhosilva35997@gmail.com>
2022-07-18 16:19:01 -0300

    erro concertado
    erro concertado

--- src/screens/Conteudo.js
+++ src/screens/Conteudo.js
@@ -5,7 +5,6 @@ import {useNavigation} from '@react-navigation/native';
 import IconFontAwesome from "react-native-vector-icons/FontAwesome";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-<<<<<<< HEAD
 import Encanto from "../../assets/Encanto/encanto.jpeg";
 import Maribel from "../../assets/Encanto/maribel.jpg";
 import Isabela from "../../assets/Encanto/isabela.jpg";
@@ -13,9 +12,6 @@ import Bruno from "../../assets/Encanto/bruno.jpg";
 import Luisa from "../../assets/Encanto/luisa.jpg";
 import Pepa from "../../assets/Encanto/pepa.jpg";
 import Dolores from "../../assets/Encanto/dolores.jpg";
-=======
-import Encanto from "../../assets/encanto.jpeg";
->>>>>>> 8db6a70b3a23702e56474ce29840a69c8d53ab56
 
 export default function Conteudos() {
 
@@ -53,7 +49,6 @@ export default function Conteudos() {
                         <View style={estilos.elencoScrowll}>
                             <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
                                 <View style={estilos.personagem}>
-<<<<<<< HEAD
                                     <Image  source={Maribel} style={estilos.elencoImg}/>
                                     <Text style={estilos.textoConteudo}>Mirabel Madrigal</Text>
                                 </View>
@@ -76,26 +71,6 @@ export default function Conteudos() {
                                 <View style={estilos.personagem}>
                                     <Image  source={Dolores} style={estilos.elencoImg}/>
                                     <Text style={estilos.textoConteudo}>Dolores Madrigal</Text>
-=======
-                                    <Image  source={Encanto} style={estilos.elencoImg}/>
-                                    <Text style={estilos.textoConteudo}>Encanto</Text>
-                                </View>
-                                <View style={estilos.personagem}>
-                                    <Image  source={Encanto} style={estilos.elencoImg}/>
-                                    <Text style={estilos.textoConteudo}>Encanto</Text>
-                                </View>
-                                <View style={estilos.personagem}>
-                                    <Image  source={Encanto} style={estilos.elencoImg}/>
-                                    <Text style={estilos.textoConteudo}>Encanto</Text>
-                                </View>
-                                <View style={estilos.personagem}>
-                                    <Image  source={Encanto} style={estilos.elencoImg}/>
-                                    <Text style={estilos.textoConteudo}>Encanto</Text>
-                                </View>
-                                <View style={estilos.personagem}>
-                                    <Image  source={Encanto} style={estilos.elencoImg}/>
-                                    <Text style={estilos.textoConteudo}>Encanto</Text>
->>>>>>> 8db6a70b3a23702e56474ce29840a69c8d53ab56
                                 </View>
                             </ScrollView>
                         </View>
